a2b2beeccc1d1e5c505cd88b70b0e48963986042
pblvicente <pvicentebusiness@gmail.com>
2025-01-13 13:41:42 +0100

    [ADD] Incluido el componente BrandSelector en ItemsView

--- frontend/src/views/ItemsView.vue
+++ frontend/src/views/ItemsView.vue
@@ -1,5 +1,10 @@
 <template>    
-    <main class="container py-5">   
+    <main class="container py-5">
+      <section class="row py-2">
+        <div class="col-6">
+          <BrandSelector />
+        </div>     
+      </section>
       <ItemsComponent />
     </main>
 </template>
