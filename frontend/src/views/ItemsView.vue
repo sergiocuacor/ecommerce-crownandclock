055eadce26588e86b8bbe6de84865f2cc1c0258e
pblvicente <pvicentebusiness@gmail.com>
2025-01-13 14:00:32 +0100

    [ADD] Añadida la funcionalidad de BrandSelector en ItemsView

--- frontend/src/views/ItemsView.vue
+++ frontend/src/views/ItemsView.vue
@@ -2,9 +2,26 @@
     <main class="container py-5">
       <section class="row py-2">
         <div class="col-6">
-          <BrandSelector />
+          <BrandSelector @brandSelected="handleBrandSelected" />
         </div>     
+      </section>
+      <section class="row py-2">
+        <div class="col-12">
+          <h1>{{ 'Marca seleccionada: ' + selectedBrandId }}</h1>
+        </div>
       </section>
       <ItemsComponent />
     </main>
 </template>
+
+<script setup>
+
+    import { ref } from 'vue';
+
+    const selectedBrandId = ref(null);
+
+    const handleBrandSelected = (brandId) => {
+        selectedBrandId.value = brandId;
+    };
+    
+</script>
