4fdc59f8ca936ca1ffe70af8a4b43ec47b148be3
pblvicente <pvicentebusiness@gmail.com>
2025-01-15 10:12:29 +0100

    [MOD] Cambiada la estructura de ItemComponent usando ElementComponent

--- frontend/src/components/items/ItemComponent.vue
+++ frontend/src/components/items/ItemComponent.vue
@@ -1,101 +1,51 @@
 <template>
 
-    <!-- COMPONENT -->
-    <article v-if="!loading && !error" class="border border-3 rounded"> 
-        <div class="position-relative bg-white">
-            <router-link :to="`/products/${item.id}`" class="ratio ratio-1x1">
-                <img class="object-fit-contain p-4" :src="item.image" :alt="item.id" loading="lazy" />
-            </router-link>
-
-            <!-- Top-left position -->
-            <div class="position-absolute top-0 start-0">
-                <RatingComponent v-if="item.rating" :itemRating="item.rating" />
+    <!-- SUCCESS -->
+    <ElementComponent v-if="!loading && !error" :toRoute="{ name: 'product', params: { mask: item.id } }" :imgSrc="item.image" :imgAlt="`${item.id}`">
+        <template #top-left>
+            <RatingComponent v-if="item.rating" :itemRating="item.rating" />
+        </template>
+        <template #footer>
+            <div class="col text-truncate">
+                {{ 'Disponibles: ' + item.rating.count }}
             </div>
-
-            <!-- Top-middle position -->
-            <div class="position-absolute top-0 start-50 translate-middle-x">
-
+            <div class="col-12 col-md-auto">
+                {{ item.price + '&#8364' }}
             </div>
+        </template>        
+    </ElementComponent>
 
-            <!-- Top-right position -->
-            <div class="position-absolute top-0 end-0">
-
+    <!-- LOADING -->
+    <ElementComponent v-if="loading">
+        <template #middle-center>
+            <div class="spinner-border">
+                <span class="visually-hidden">{{ 'Loading...' }}</span>
             </div>
-
-            <!-- Middle-left position -->
-            <div class="position-absolute top-50 start-0 translate-middle-y">
-
+        </template>
+        <template #footer>
+            <div class="col text-truncate placeholder-glow">
+                <span class="placeholder">{{ 'Disponibles: ?' }}</span>
             </div>
-
-            <!-- Middle-center position -->
-            <div class="position-absolute top-50 start-50 translate-middle">
-
+            <div class="col-12 col-md-auto placeholder-glow">
+                <span class="placeholder">{{ '??????' }}</span>
             </div>
-
-            <!-- Middle-right position -->
-            <div class="position-absolute top-50 end-0 translate-middle-y">
-
-            </div>
-
-            <!-- Bottom-left position -->
-            <div class="position-absolute bottom-0 start-0">
-
-            </div>
-
-            <!-- Bottom-middle position -->
-            <div class="position-absolute bottom-0 start-50 translate-middle-x">
-
-            </div>
-
-            <!-- Bottom-right position -->
-            <div class="position-absolute bottom-0 end-0">
-
-            </div>
-
-            <!-- Footer Section -->
-            <div class="d-block bg-primary-subtle">
-                <div class="row p-2">
-                    <div class="col text-truncate">
-                        {{ 'Disponibles: ' + item.rating.count }}
-                    </div>
-                    <div class="col-12 col-md-auto">
-                        {{ item.price + '&#8364'}}
-                    </div>
-                </div>
-            </div>
-        </div>
-    </article>
-
-     <!-- LOADING -->
-    <article v-if="loading" class="border border-3 rounded">  
-        <div class="position-relative bg-white">
-            <div class="ratio ratio-1x1">
-
-            </div>
-
-            <!-- Middle-center position -->
-            <div class="position-absolute top-50 start-50 translate-middle">
-                <div class="spinner-border">
-                    <span class="visually-hidden">{{ 'Loading...' }}</span>
-                </div>
-            </div>
-
-            <!-- Footer Section -->
-            <div class="d-block bg-primary-subtle">
-                <div class="row p-2">
-                    <div class="col text-truncate placeholder-glow">
-                        <span class="placeholder">{{ 'Disponibles: ?' }}</span>
-                    </div>
-                    <div class="col-12 col-md-auto placeholder-glow">
-                        <span class="placeholder">{{ '??????' }}</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </article>
+        </template>      
+    </ElementComponent>
 
     <!-- ERROR -->
-    <article v-if="error">{{ error }}</article>
+    <ElementComponent v-if="error">
+        <template #middle-center>
+            <i class="bi bi-exclamation-diamond-fill tw-text-red-500 fs-1"></i>
+        </template>
+        <template #footer>
+            <div class="col text-truncate placeholder-glow">
+                <span class="placeholder">{{ 'Disponibles: ?' }}</span>
+            </div>
+            <div class="col-12 col-md-auto placeholder-glow">
+                <span class="placeholder">{{ '??????' }}</span>
+            </div>
+        </template>      
+    </ElementComponent>
 
 </template>
 
