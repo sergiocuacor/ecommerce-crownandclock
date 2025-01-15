0ac882684810e0f3dbe858e7e1d3d95b2758d691
pblvicente <pvicentebusiness@gmail.com>
2025-01-15 12:15:54 +0100

    [MOD] Cambiada la estructura de CategoryComponent usando ElementComponent

--- frontend/src/components/categories/CategoryComponent.vue
+++ frontend/src/components/categories/CategoryComponent.vue
@@ -1,95 +1,42 @@
 <template>
 
-    <!-- COMPONENT -->
-    <article v-if="!loading && !error" class="border border-3 rounded"> 
-        <div class="position-relative bg-white">
-            <router-link :to="{ name: 'category', params: { mask: category.id } }" class="ratio ratio-1x1">
-                <img class="object-fit-contain p-4" :src="category.image" :alt="category.id" loading="lazy" />
-            </router-link>
-
-            <!-- Top-left position -->
-            <div class="position-absolute top-0 start-0">
-
+    <!-- SUCCESS -->
+    <ElementComponent v-if="!loading && !error" :toRoute="{ name: 'category', params: { mask: category.id } }" :imgSrc="category.image" :imgAlt="`${category.id}`">
+        <template #footer>
+            <div class="col-12">
+                {{ category.name }};
             </div>
-
-            <!-- Top-middle position -->
-            <div class="position-absolute top-0 start-50 translate-middle-x">
-
-            </div>
-
-            <!-- Top-right position -->
-            <div class="position-absolute top-0 end-0">
-
-            </div>
-
-            <!-- Middle-left position -->
-            <div class="position-absolute top-50 start-0 translate-middle-y">
-
-            </div>
-
-            <!-- Middle-center position -->
-            <div class="position-absolute top-50 start-50 translate-middle">
-
-            </div>
-
-            <!-- Middle-right position -->
-            <div class="position-absolute top-50 end-0 translate-middle-y">
-
-            </div>
-
-            <!-- Bottom-left position -->
-            <div class="position-absolute bottom-0 start-0">
-
-            </div>
-
-            <!-- Bottom-middle position -->
-            <div class="position-absolute bottom-0 start-50 translate-middle-x">
-
-            </div>
-
-            <!-- Bottom-right position -->
-            <div class="position-absolute bottom-0 end-0">
-
-            </div>
-
-            <!-- Footer Section -->
-            <div class="d-block bg-primary-subtle">
-                <div class="row p-2">
-                    <div class="col-12">
-                        {{ category.name }};
-                    </div>
-                </div>
-            </div>
-        </div>
-    </article>
+        </template>        
+    </ElementComponent>
 
     <!-- LOADING -->
-    <article v-if="loading" class="border border-3 rounded">  
-        <div class="position-relative bg-white">
-            <div class="ratio ratio-1x1">
-
+    <ElementComponent v-if="loading">
+        <template #middle-center>
+            <div class="spinner-border">
+                <span class="visually-hidden">{{ 'Loading...' }}</span>
             </div>
-
-            <!-- Middle-center position -->
-            <div class="position-absolute top-50 start-50 translate-middle">
-                <div class="spinner-border">
-                    <span class="visually-hidden">{{ 'Loading...' }}</span>
-                </div>
+        </template>
+        <template #footer>
+            <div class="col-12 text-truncate placeholder-glow">
+                <span class="placeholder">{{ '??????????' }}</span>
             </div>
-
-            <!-- Footer Section -->
-            <div class="d-block bg-primary-subtle">
-                <div class="row p-2">
-                    <div class="col-12 text-truncate placeholder-glow">
-                        <span class="placeholder">{{ '??????????' }}</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </article>
+        </template>      
+    </ElementComponent>
 
     <!-- ERROR -->
-    <article v-if="error">{{ error }}</article>
+    <ElementComponent v-if="error">
+        <template #middle-center>
+            <i class="bi bi-exclamation-diamond-fill tw-text-red-500 fs-1"></i>
+        </template>
+        <template #footer>
+            <div class="col text-truncate placeholder-glow">
+                <span class="placeholder">{{ 'Disponibles: ?' }}</span>
+            </div>
+            <div class="col-12 col-md-auto placeholder-glow">
+                <span class="placeholder">{{ '??????' }}</span>
+            </div>
+        </template>      
+    </ElementComponent>
 
 </template>
 
