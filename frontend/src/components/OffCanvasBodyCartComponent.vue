071e84e7a49e2554b8d850efb28c207f94eaacad
pblvicente <pvicentebusiness@gmail.com>
2025-01-16 13:13:04 +0100

    [ADD] Funcionalidad básica de OffCanvasBodyCartComponent

--- frontend/src/components/OffCanvasBodyCartComponent.vue
+++ frontend/src/components/OffCanvasBodyCartComponent.vue
@@ -1,3 +1,15 @@
 <template>
-    TO DO
+    <ul class="list-group">        
+        <li v-for="item in items" :key="item.id" class="list-group-item">
+            {{ item.title }}
+        </li>
+    </ul>
 </template>
+
+<script setup>
+
+    import { useCartStore } from '../store/cart.js';
+
+    const items = useCartStore().items;
+
+</script>
