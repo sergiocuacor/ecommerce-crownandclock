a704cb807010e1b5ee855d30b48a61b286aee979
pblvicente <pvicentebusiness@gmail.com>
2025-01-16 18:28:44 +0100

    [ADD] Mejorado el código que gestiona la cesta, añadiendo el guardado de los productos en el localStorage

--- frontend/src/store/cart.js
+++ frontend/src/store/cart.js
@@ -1,32 +1,72 @@
 import { defineStore } from 'pinia';
 
 export const useCartStore = defineStore('cart', {
+
   state: () => ({
-    items: [],
+
+    items: JSON.parse(localStorage.getItem('cartItems')) || [],
+
   }),
+
   actions: {
+
     addToCart(product) {
+
       const existingItem = this.items.find(item => item.id === product.id);
+
       if (existingItem) {
+
         existingItem.quantity++;
+
       } else {
+
         this.items.push({ ...product, quantity: 1 });
+
       }
+
+      this.saveCart();
+
     },
+
     removeFromCart(productId) {
+
       this.items = this.items.filter(item => item.id !== productId);
+      this.saveCart();
+
     },
+
     increaseQuantity(productId) {
+
       const item = this.items.find(item => item.id === productId);
+
       if (item) {
+
         item.quantity++;
+        this.saveCart();
+
       }
+
     },
+
     decreaseQuantity(productId) {
+
       const item = this.items.find(item => item.id === productId);
+
       if (item && item.quantity > 1) {
+
         item.quantity--;
+        this.saveCart();
+
       }
+
     },
+
+    saveCart() {
+
+      localStorage.setItem('cartItems', JSON.stringify(this.items));
+
+    },
+
   },
+  
 });
