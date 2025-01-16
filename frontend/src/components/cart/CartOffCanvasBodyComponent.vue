af3a89dc7842e9f6e261151cf6bc89527d896269
pblvicente <pvicentebusiness@gmail.com>
2025-01-16 19:05:53 +0100

    [REF] Movido OffCanvasBodyCartComponent a la carpeta cart y renombrado a CartOffCanvasBodyComponent

---
+++ frontend/src/components/cart/CartOffCanvasBodyComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <ul class="list-group list-group-flush">
+        <li v-for="item in cartStore.items" :key="item.id" class="list-group-item">
+            {{ item.title }}
+        </li>
+    </ul>
+</template>
+
+<script setup>
+
+    import { useCartStore } from '../../store/cart.js';
+
+    const cartStore = useCartStore();
+
+</script>
