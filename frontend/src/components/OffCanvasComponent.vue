acd1a5e2928de1614ec8beba60bb4b96bf9f2e37
pblvicente <pvicentebusiness@gmail.com>
2025-01-17 00:28:57 +0100

    [MOD] Ajustada la estructura del script de OffCanvasComponent a Vue3

--- frontend/src/components/OffCanvasComponent.vue
+++ frontend/src/components/OffCanvasComponent.vue
@@ -1,24 +1,26 @@
 <template>
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
         <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasNavbarLabel"><i class="bi bi-hexagon-fill"> Sidebar</i></h5>
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
+                <i v-if="content === 'cart'" class="bi bi-cart-fill">{{ ' Carrito' }}</i>
+            </h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
-            <OffCanvasBodyCartComponent v-if="content === 'cart'" />
-            <OffCanvasBodyControlPanelComponent v-if="content === 'controlPanel'" />
-            <p v-if="content === ''">Selecciona un contenido.</p>
+            <CartOffCanvasBodyComponent v-if="content === 'cart'" />
+            <UserControlPanelOffCanvasBodyComponent v-if="content === 'controlPanel'" />
         </div>
     </div>
 </template>
 
-<script>
-    export default {
-        props: {
-            content: {
+<script setup>
+
+    const props = defineProps({
+        content: {
             type: String,
-            default: "",
-            },
+            default: '',
+            required: true,
         },
-    };
+    });
+
 </script>
