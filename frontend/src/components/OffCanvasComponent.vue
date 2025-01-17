855998f74a8eb8a93fb70cb1c2ce262398e80342
pblvicente <pvicentebusiness@gmail.com>
2025-01-17 17:04:37 +0100

    [MOD] Cambiada la funcionalidad del componente OffCanvasComponent para que su content venga del localStorage

--- frontend/src/components/OffCanvasComponent.vue
+++ frontend/src/components/OffCanvasComponent.vue
@@ -2,25 +2,23 @@
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
         <div class="offcanvas-header">
             <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
-                <i v-if="content === 'cart'" class="bi bi-cart-fill">{{ ' Carrito' }}</i>
+                <i v-if="offCanvasContent === 'cart'" class="bi bi-cart-fill">{{ ' Carrito' }}</i>
             </h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
-            <CartOffCanvasBodyComponent v-if="content === 'cart'" />
-            <UserControlPanelOffCanvasBodyComponent v-if="content === 'controlPanel'" />
+            <CartOffCanvasBodyComponent v-if="offCanvasContent === 'cart'" />
+            <UserControlPanelOffCanvasBodyComponent v-if="offCanvasContent === 'controlPanel'" />
         </div>
     </div>
 </template>
 
 <script setup>
 
-    const props = defineProps({
-        content: {
-            type: String,
-            default: '',
-            required: true,
-        },
-    });
+    import { useUtilsStore } from '../services/utils.js';
+
+    const utilsStore = useUtilsStore();
+
+    const offCanvasContent = utilsStore.offCanvasContent;
 
 </script>
