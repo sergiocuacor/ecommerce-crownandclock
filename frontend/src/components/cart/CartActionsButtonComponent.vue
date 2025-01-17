710853b253de76a02d708be5d64db6c06a7d18ef
pblvicente <pvicentebusiness@gmail.com>
2025-01-17 17:14:28 +0100

    [MOD] Ahora se abre el OffCanvasComponent al hacer click en los botones de CartActionsButtonComponent

--- frontend/src/components/cart/CartActionsButtonComponent.vue
+++ frontend/src/components/cart/CartActionsButtonComponent.vue
@@ -1,17 +1,19 @@
 <template>
     <div class="btn-group m-1" aria-label="Cart Buttons">
-        <button class="btn btn-sm btn-success" @click="addToCart">
+        <button class="btn btn-sm btn-success" @click="addToCart" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
             <i class="bi bi-bag-plus-fill"></i>
         </button>
-        <button class="btn btn-sm btn-danger" @click="removeFromCart">
+        <button type="button" class="btn btn-sm btn-danger" @click="removeFromCart" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" :disabled="isDisabled">
             <i class="bi bi-bag-dash-fill"></i>
-        </button>
+        </button>       
     </div>  
 </template>
 
 <script setup>
 
+    import { ref, onMounted } from 'vue';
     import { useCartStore } from '../../store/cart.js';
+    import { useUtilsStore } from '../../services/utils.js';
     
     const props = defineProps({
         item: {
@@ -19,15 +21,26 @@
             required: true,
         },
     });
-    
+
     const cartStore = useCartStore();
+    const utilsStore = useUtilsStore();
+
+    const isDisabled = ref(true);
 
     const addToCart = () => {
         cartStore.addToCart(props.item);
+        utilsStore.setOffCanvasContent('cart');
+        isDisabled.value = false;
     };
 
     const removeFromCart = () => {
         cartStore.removeFromCart(props.item.id);
+        utilsStore.setOffCanvasContent('cart');
+        isDisabled.value = true;
     };
+
+    onMounted(() => {
+        isDisabled.value = !cartStore.items.some(item => item.id === props.item.id);
+    });
 
 </script>
