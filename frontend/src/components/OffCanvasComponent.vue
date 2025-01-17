e8d9b8979fbfdeef1d5d6db7aa67a1d74db16ec4
pblvicente <pvicentebusiness@gmail.com>
2025-01-17 17:10:12 +0100

    [FIX] Llamada directa a offCanvasContent de utilsStore en el template, evitando crear un variable

--- frontend/src/components/OffCanvasComponent.vue
+++ frontend/src/components/OffCanvasComponent.vue
@@ -2,13 +2,13 @@
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
         <div class="offcanvas-header">
             <h5 class="offcanvas-title" id="offcanvasNavbarLabel">
-                <i v-if="offCanvasContent === 'cart'" class="bi bi-cart-fill">{{ ' Carrito' }}</i>
+                <i v-if="utilsStore.offCanvasContent === 'cart'" class="bi bi-cart-fill">{{ ' Carrito' }}</i>
             </h5>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div class="offcanvas-body">
-            <CartOffCanvasBodyComponent v-if="offCanvasContent === 'cart'" />
-            <UserControlPanelOffCanvasBodyComponent v-if="offCanvasContent === 'controlPanel'" />
+            <CartOffCanvasBodyComponent v-if="utilsStore.offCanvasContent === 'cart'" />
+            <UserControlPanelOffCanvasBodyComponent v-if="utilsStore.offCanvasContent === 'controlPanel'" />
         </div>
     </div>
 </template>
@@ -18,7 +18,5 @@
     import { useUtilsStore } from '../services/utils.js';
 
     const utilsStore = useUtilsStore();
-
-    const offCanvasContent = utilsStore.offCanvasContent;
 
 </script>
