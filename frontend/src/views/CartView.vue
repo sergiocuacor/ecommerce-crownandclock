bd343f58f1463c058bfae78cd34da3f0a716ac79
pblvicente <pvicentebusiness@gmail.com>
2025-01-21 09:41:02 +0100

    [MOD] Cambiado que se muestra en CartView si el carrito esta vacío o no

--- frontend/src/views/CartView.vue
+++ frontend/src/views/CartView.vue
@@ -1,8 +1,33 @@
 <template>
 
-    <main class="container tw-py-20">
-      <div class="tw-text-2xl tw-text-center tw-p-5 tw-bg-gray-300 tw-border-2 tw-rounded-t-lg">{{ 'Carrito' }}</div>
-      <CartItemsComponent />
-    </main>
+  <!-- FULL -->
+  <main v-if="cartStore.items.length > 0" class="container tw-pt-20 tw-pb-2.5">
+    <div class="tw-text-2xl tw-text-center tw-p-5 tw-bg-gray-300 tw-border-2 tw-rounded-t-lg">
+      {{ 'Carrito' }}
+    </div>
+    <CartItemsComponent />
+  </main>
+    
+  <!-- EMPTY -->    
+  <DynamicHeightDivComponent v-else>
+    <template #header>
+        <div class="tw-font-mono tw-text-md lg:tw-text-xl tw-bg-gray-200 tw-px-2 tw-rounded">
+            {{ 'Carrito Vacío' }}
+        </div>
+    </template>
+    <template #footer >
+        <div class="tw-font-mono tw-text-xs lg:tw-text-base">
+            {{ 'No hay productos' }}
+        </div>
+    </template>
+  </DynamicHeightDivComponent>
 
 </template>
+
+<script setup>
+  
+  import { useCartStore } from '../store/cart.js';    
+
+  const cartStore = useCartStore();
+
+</script>
