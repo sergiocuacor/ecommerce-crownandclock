29015b2c318c90932b1349d5c54cdb9cc2fbb6ac
pblvicente <pvicentebusiness@gmail.com>
2025-01-21 10:26:26 +0100

    [MOD] Cambiado el resultado del total del carrito para que solo tenga 2 decimales en CartOffCanvasBodyComponent

--- frontend/src/components/cart/CartOffCanvasBodyComponent.vue
+++ frontend/src/components/cart/CartOffCanvasBodyComponent.vue
@@ -1,4 +1,6 @@
 <template>
+
+    <!-- FULL -->
     <ul v-if="cartStore.items.length > 0" class="list-group list-group-flush">
         <li v-for="item in cartStore.items" :key="item.id" class="list-group-item">            
             <div class="tw-flex tw-items-center tw-gap-4">
@@ -20,19 +22,22 @@
                     </div>
                 </div>
             </div>
-        </li>
-        
+        </li>        
     </ul>
+
     <div v-if="cartStore.items.length > 0" class="">            
         <div class="tw-text-2xl tw-text-center tw-p-5 space-y-2.5 tw-bg-gray-300 tw-border-2 tw-rounded-lg">
-            {{ 'Total: ' + cartTotal + '&#8364' }}         
+            {{ 'Total: ' + cartTotal + '&#8364' }}     
         </div>
     </div>
+
+    <!-- EMPTY -->
     <div v-else class="tw-w-full tw-h-full tw-relative">
         <div class="tw-absolute tw-top-1/2 tw-left-1/2 tw-transform tw--translate-x-1/2 tw--translate-y-1/2 tw-bg-gray-200 tw-px-2 tw-rounded">
             {{ 'Vacío' }}
         </div>
     </div>
+
 </template>
 
 <script setup>
@@ -43,7 +48,7 @@
     const cartStore = useCartStore();
 
     const cartTotal = computed(() => {
-        return cartStore.items.reduce((total, item) => total + item.price * item.quantity, 0);
+        return cartStore.items.reduce((total, item) => total + item.price * item.quantity, 0).toFixed(2);
     });
 
 </script>
