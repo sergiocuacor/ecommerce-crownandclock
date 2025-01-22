da573149cb14f2ceacaa9e3419e68c034cc8ca8d
pblvicente <pvicentebusiness@gmail.com>
2025-01-22 12:27:22 +0100

    [MOD] Añadido un estilo para el borde en CartItemsComponent

--- frontend/src/components/cart/CartItemsComponent.vue
+++ frontend/src/components/cart/CartItemsComponent.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <ul v-if="cartStore.items.length > 0" class="tw-px-2.5 tw-divide-y-2 tw-divide-slate-400/25 tw-bg-gray-200 tw-border-2">
+    <ul v-if="cartStore.items.length > 0" class="tw-px-2.5 tw-divide-y-2 tw-divide-slate-400/25 tw-bg-gray-200 tw-border-2 tw-rounded-b-lg">
         <CartItemComponent v-for="item in cartStore.items" :key="item.id" :item="item"/>
     </ul>
 
