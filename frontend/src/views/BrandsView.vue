5d904bc3cd617d6a018652aa54f6e5a0da13435f
pblvicente <pvicentebusiness@gmail.com>
2025-01-22 11:30:22 +0100

    [MOD] Reorganizado BrandsView

--- frontend/src/views/BrandsView.vue
+++ frontend/src/views/BrandsView.vue
@@ -1,10 +1,14 @@
-<template>       
-    <main class="tw-relative">
-        <video class="tw-object-cover tw-w-full" src="../../video1.mp4" autoplay loop>
+<template>
+    <main>
+        <div class="tw-relative tw-h-screen tw-overflow-hidden">
+            <video class="tw-object-cover tw-w-full tw-h-full" src="../../video1.mp4" autoplay loop>
 
-        </video>
-        <div class="tw-absolute tw-inset-0">
-            <BrandsComponent />
+            </video>
+            <div class="tw-absolute tw-top-1/2 tw-left-1/2 tw-transform tw--translate-x-1/2 tw--translate-y-1/2">
+                <p class="tw-font-semibold tw-subpixel-antialiased tw-font-mono tw-text-8xl hover:tw-text-9xl tw-tracking-widest tw-text-white">{{ 'MARCAS' }}</p>
+            </div>
         </div>
+
+        <BrandsComponent />
     </main>
 </template>
