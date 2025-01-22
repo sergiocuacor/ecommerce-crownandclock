90fa74ab6108002d69e2b557f604127389ba7c73
pblvicente <pvicentebusiness@gmail.com>
2025-01-22 09:28:37 +0100

    [FIX] Cambiada la variable cartItems que no existe por cartStore.items en el componente CartItemsComponent

--- frontend/src/components/cart/CartItemsComponent.vue
+++ frontend/src/components/cart/CartItemsComponent.vue
@@ -1,7 +1,7 @@
 <template>
 
     <ul v-if="cartStore.items.length > 0" class="tw-px-2.5 tw-divide-y-2 tw-divide-slate-400/25 tw-bg-gray-200 tw-border-2">
-        <CartItemComponent v-for="item in cartItems" :key="item.id" :item="item"/>
+        <CartItemComponent v-for="item in cartStore.items" :key="item.id" :item="item"/>
     </ul>
 
 </template>
