c572da5b4b89d4363135ee5771b9fee7fe77da42
pblvicente <pvicentebusiness@gmail.com>
2025-01-23 13:33:33 +0100

    [MOD] Mejorado el diseño del componente LoginFormComponent

--- frontend/src/components/users/LoginFormComponent.vue
+++ frontend/src/components/users/LoginFormComponent.vue
@@ -1,17 +1,24 @@
 <template>
 
-    <div class="mb-3 row">
-        <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-        <div class="col-sm-10">
-            <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+    <section
+        class="d-flex flex-column align-items-center justify-content-center h-100">       
+        <div class="p-4 rounded tw-bg-gray-200 border border-2 tw-border-gray-300 tw-space-y-2.5">
+            <div class="text-center">
+                {{ 'Login' }}
+            </div>
+            <div class="">
+                <label for="inputEmail" class="form-label">{{ 'Email address' }}</label>
+                <input type="email" class="form-control" id="inputEmail" placeholder="name@example.com">
+            </div>
+            <div class="">
+                <label for="inputPassword" class="form-label">{{ 'Password' }}</label>
+                <input type="password" id="inputPassword" name="inputPassword" class="form-control">
+                <div id="passwordHelpBlock" class="form-text">
+                    {{ 'Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.' }}
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="mb-3 row">
-        <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-        <div class="col-sm-10">
-            <input type="password" class="form-control" id="inputPassword">
-        </div>
-    </div>
+    </section>
 
 </template>
 
