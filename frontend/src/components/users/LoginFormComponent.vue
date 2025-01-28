eb8c3cbb417eb3b980d0e3df6e627ae9ec7058ca
pblvicente <pvicentebusiness@gmail.com>
2025-01-28 11:49:29 +0100

    [MOD] Cambiado el estilo y el texto de los labels en LoginFormComponent

--- frontend/src/components/users/LoginFormComponent.vue
+++ frontend/src/components/users/LoginFormComponent.vue
@@ -2,16 +2,16 @@
 
     <section
         class="d-flex flex-column align-items-center justify-content-center h-100">       
-        <div class="p-4 rounded tw-bg-gray-200 border border-2 tw-border-gray-300 tw-space-y-2.5">
+        <div class="p-4 rounded tw-bg-gray-200/75 border border-2 tw-border-gray-300/50 tw-space-y-2.5">
             <div class="text-center">
                 {{ 'Login' }}
             </div>
             <div class="">
-                <label for="inputEmail" class="form-label">{{ 'Email address' }}</label>
+                <label for="inputEmail" class="form-label">{{ 'Email o usuario' }}</label>
                 <input type="email" class="form-control" id="inputEmail" placeholder="name@example.com">
             </div>
             <div class="">
-                <label for="inputPassword" class="form-label">{{ 'Password' }}</label>
+                <label for="inputPassword" class="form-label">{{ 'Contraseña' }}</label>
                 <input type="password" id="inputPassword" name="inputPassword" class="form-control">
                 <div id="passwordHelpBlock" class="form-text">
                     {{ 'Your password must be 8-20 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji.' }}
