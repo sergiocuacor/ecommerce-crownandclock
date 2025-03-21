e0f72500d76fb74cdc71ebaa68d1eeca94911159
pblvicente <pvicentebusiness@gmail.com>
2025-03-21 11:58:46 +0100

    [ADD] Creado UserProfileView con estructura básica

---
+++ frontend/src/views/UserProfileView.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <main class="container tw-pt-16 lg:tw-pt-14">
+        
+        <section class="row">
+
+            <article class="col-12 col-lg-3">
+
+                <UserProfileImageComponent />
+
+            </article>        
+
+            <article class="col-12 col-lg-9">
+
+            </article>
+        
+        </section>
+
+        <section class="row">
+            
+            <article class="col-12">
+
+
+            </article>
+
+        </section>        
+
+    </main>
+    
+</template>
