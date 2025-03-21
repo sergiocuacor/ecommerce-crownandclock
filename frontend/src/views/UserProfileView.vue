20720431a4212c5ee3beb6946de4fc137140b398
pblvicente <pvicentebusiness@gmail.com>
2025-03-21 14:41:40 +0100

    [MOD] Añadidos datos de prueba para UserProfileView

--- frontend/src/views/UserProfileView.vue
+++ frontend/src/views/UserProfileView.vue
@@ -12,6 +12,9 @@
 
             <article class="col-12 col-lg-9">
 
+                <p class="fs-3 fw-bold">{{ user.nombre }} {{ user.apellidos }}</p>
+                <p class="fs-5">{{ user.email }}</p>
+
             </article>
         
         </section>
@@ -21,6 +24,7 @@
             <article class="col-12">
 
 
+                
             </article>
 
         </section>        
@@ -28,3 +32,62 @@
     </main>
     
 </template>
+
+<script setup>
+
+    import { ref } from 'vue';
+
+    const user = ref([]);
+
+    user.value = {
+        id: 1,
+        nombre: 'Juan',
+        apellidos: 'Perez',
+        email: 'mondongo@sohotmail.com',
+        password: 'aaaaaa',
+        direccion: 'c/ de la piruleta, 69',
+        telefono: '888888888',
+        fechaRegistro: '12/12/2021',
+        listaPedidos: [
+            {
+                id: 1,
+                fecha: '12/12/2021',
+                total: 100,
+                productos: [
+                    {
+                        id: 1,
+                        nombre: 'Producto 1',
+                        precio: 10,
+                        cantidad: 2,
+                    },
+                    {
+                        id: 2,
+                        nombre: 'Producto 2',
+                        precio: 20,
+                        cantidad: 3,
+                    },
+                ],
+            },
+            {
+                id: 2,
+                fecha: '13/12/2021',
+                total: 200,
+                productos: [
+                    {
+                        id: 3,
+                        nombre: 'Producto 3',
+                        precio: 30,
+                        cantidad: 4,
+                    },
+                    {
+                        id: 4,
+                        nombre: 'Producto 4',
+                        precio: 40,
+                        cantidad: 5,
+                    },
+                ],
+            },
+        ],
+    };
+
+</script>
