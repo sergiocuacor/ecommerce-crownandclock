03da662237b19f958cd15baf20a5c5c30275a855
pblvicente <pvicentebusiness@gmail.com>
2025-03-26 16:17:52 +0100

    [MOD] Cambio a la API real y mejorado el código de las llamadas

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,41 +1,7 @@
 import axios from 'axios';
 
-const apiClient = axios.create({
-  baseURL: 'https://fakestoreapi.com',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
-
-export default {
-  getItem(mask) {
-    return apiClient.get(`/products/${mask}`);
-  },
-  getItems() {
-    return apiClient.get('/products');
-  },
-  getBrand(mask) {
-    return apiClient.get(`/brands/${mask}`);
-  },
-  getBrands() {
-    return apiClient.get('/brands');
-  },
-  getCategory(mask) {
-    return apiClient.get(`/categories/${mask}`);
-  },
-  getCategories() {
-    return apiClient.get('/products/categories');
-  },
-  getItemsFromBrand(mask) {
-    return apiClient.get(`/products/brand/${mask}`);
-  }, 
-  getItemsFromCategory(mask) {
-    return apiClient.get(`/products/category/${mask}`);
-  },  
-};
-
 // const apiClient = axios.create({
-//   baseURL: 'https://localhost:8080',
+//   baseURL: 'https://fakestoreapi.com',
 //   headers: {
 //     'Content-Type': 'application/json',
 //   },
@@ -46,10 +12,7 @@ export default {
 //     return apiClient.get(`/products/${mask}`);
 //   },
 //   getItems() {
-//     return apiClient.get('/product/all');
-//   },
-//   getItemsPageable(page = 0, size = 12) {
-//     return apiClient.get('/product', { params: { page, size, sort: 'name',}, });
+//     return apiClient.get('/products');
 //   },
 //   getBrand(mask) {
 //     return apiClient.get(`/brands/${mask}`);
@@ -70,3 +33,34 @@ export default {
 //     return apiClient.get(`/products/category/${mask}`);
 //   },  
 // };
+
+const apiClient = axios.create({
+  baseURL: 'http://localhost:8080',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+async function apiCall(method, url, params = {}, data = {}) {
+  try {
+    const response = await apiClient({ method, url, params, data });
+    return { success: true, data: response.data };
+  } catch (err) {
+    return { success: false, error: err.response?.data || 'Error desconocido' };
+  }
+}
+
+export default {
+  getApiBaseURL() {
+    return apiClient.defaults.baseURL;
+  },
+  getItem(mask) {
+    return apiCall('get', `/product/${mask}`);
+  },
+  getItems() {
+    return apiCall('get', '/product/all');
+  },
+  getItemsPageable(page = 0, size = 12, sort = 'name') {
+    return apiCall('get', '/product', { page, size, sort });
+  },  
+};
