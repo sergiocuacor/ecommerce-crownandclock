75a93efefbaaabb848175886768fd101b35983f9
pblvicente <pvicentebusiness@gmail.com>
2025-03-28 15:10:30 +0100

    [MOD] Arreglar paginación en ItemPaginationComponent

--- frontend/src/components/items/ItemPaginationComponent.vue
+++ frontend/src/components/items/ItemPaginationComponent.vue
@@ -22,7 +22,7 @@
         </div>
 
         <!-- DESKTOP -->
-        <div class="tw-hidden sm:tw-flex sm:tw-flex-1 sm:tw-items-center sm:tw-justify-between">
+        <div class="tw-hidden sm:tw-flex sm:tw-flex-1 sm:tw-flex-col md:tw-flex-row sm:tw-items-center sm:tw-justify-between sm:tw-gap-2">
             <div>
                 <p class="tw-text-sm tw-text-gray-700">
                     {{ 'Mostrando de' }}
@@ -37,23 +37,41 @@
             <div>
                 <nav class="tw-isolate tw-inline-flex -tw-space-x-px tw-rounded-md tw-shadow-xs" aria-label="Pagination">
 
-                    <button @click="firstPage" :disabled="currentPage <= 0" class="tw-relative tw-inline-flex tw-items-center tw-rounded-l-md tw-px-2 tw-py-2 tw-text-gray-400 tw-ring-1 tw-ring-gray-300 tw-ring-inset hover:tw-bg-gray-50 focus:tw-z-20 focus:tw-outline-offset-0">
+                    <!-- FIRST & PREVIOUS -->
+                    <button @click="firstPage" :disabled="currentPage <= 1" class="tw-relative tw-inline-flex tw-items-center tw-rounded-l-md tw-px-2 tw-py-2 tw-text-gray-400 tw-ring-1 tw-ring-gray-300 tw-ring-inset hover:tw-bg-gray-50 focus:tw-z-20 focus:tw-outline-offset-0">
                         <span class="tw-sr-only">{{ 'Primera Página' }}</span>
                         <i class="bi bi-chevron-double-left"></i>
                     </button>
-                    <button @click="prevPage" :disabled="currentPage <= 0" class="tw-relative tw-inline-flex tw-items-center tw-px-2 tw-py-2 tw-text-gray-400 tw-ring-1 tw-ring-gray-300 tw-ring-inset hover:tw-bg-gray-50 focus:tw-z-20 focus:tw-outline-offset-0">
+                    <button @click="prevPage" :disabled="currentPage <= 1" class="tw-relative tw-inline-flex tw-items-center tw-px-2 tw-py-2 tw-text-gray-400 tw-ring-1 tw-ring-gray-300 tw-ring-inset hover:tw-bg-gray-50 focus:tw-z-20 focus:tw-outline-offset-0">
                         <span class="tw-sr-only">{{ 'Anterior' }}</span>
                         <i class="bi bi-chevron-left"></i>
                     </button>
 
-                    <!-- <a href="#" aria-current="page" class="tw-relative tw-z-10 tw-inline-flex tw-items-center tw-bg-indigo-600 tw-px-4 tw-py-2 tw-text-sm tw-font-semibold tw-text-white focus:tw-z-20 focus-visible:tw-outline-2 focus-visible:tw-outline-offset-2 focus-visible:tw-outline-indigo-600">1</a>
-                    <a href="#" class="tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-text-sm tw-font-semibold tw-text-gray-900 tw-ring-1 tw-ring-gray-300 tw-ring-inset hover:tw-bg-gray-50 focus:tw-z-20 focus:tw-outline-offset-0">2</a>
-                    <a href="#" class="tw-relative tw-hidden tw-items-center tw-px-4 tw-py-2 tw-text-sm tw-font-semibold tw-text-gray-900 tw-ring-1 tw-ring-gray-300 tw-ring-inset hover:tw-bg-gray-50 focus:tw-z-20 focus:tw-outline-offset-0 md:tw-inline-flex">3</a>
-                    <span class="tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-text-sm tw-font-semibold tw-text-gray-700 tw-ring-1 tw-ring-gray-300 tw-ring-inset focus:tw-outline-offset-0">...</span>
-                    <a href="#" class="tw-relative tw-hidden tw-items-center tw-px-4 tw-py-2 tw-text-sm tw-font-semibold tw-text-gray-900 tw-ring-1 tw-ring-gray-300 tw-ring-inset hover:tw-bg-gray-50 focus:tw-z-20 focus:tw-outline-offset-0 md:tw-inline-flex">8</a>
-                    <a href="#" class="tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-text-sm tw-font-semibold tw-text-gray-900 tw-ring-1 tw-ring-gray-300 tw-ring-inset hover:tw-bg-gray-50 focus:tw-z-20 focus:tw-outline-offset-0">9</a>
-                    <a href="#" class="tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-text-sm tw-font-semibold tw-text-gray-900 tw-ring-1 tw-ring-gray-300 tw-ring-inset hover:tw-bg-gray-50 focus:tw-z-20 focus:tw-outline-offset-0">10</a> -->
+                    <button v-if="pageNumberButtons.buttons[0]?.condition" @click="goToAnyPage(pageNumberButtons.buttons[0]?.number)" :class="[pageNumberButtons.commonClasses, (currentPage == pageNumberButtons.buttons[0]?.number) ? pageNumberButtons.currentPageClass : pageNumberButtons.notCurrentPageClass]">
+                        {{ pageNumberButtons.buttons[0]?.number }}
+                    </button>
+                    <button v-if="pageNumberButtons.buttons[1]?.condition" @click="goToAnyPage(pageNumberButtons.buttons[1]?.number)" :class="[pageNumberButtons.commonClasses, (currentPage == pageNumberButtons.buttons[1]?.number) ? pageNumberButtons.currentPageClass : pageNumberButtons.notCurrentPageClass]">
+                        {{ pageNumberButtons.buttons[1]?.number }}
+                    </button>
+                    <button v-if="pageNumberButtons.buttons[2]?.condition" @click="goToAnyPage(pageNumberButtons.buttons[2]?.number)" :class="[pageNumberButtons.commonClasses, (currentPage == pageNumberButtons.buttons[2]?.number) ? pageNumberButtons.currentPageClass : pageNumberButtons.notCurrentPageClass]">
+                        {{ pageNumberButtons.buttons[2]?.number }}
+                    </button>
+
+                    <span v-if="totalPages >= 7 && (currentPage < (totalPages - 3))" class="tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-text-sm tw-font-semibold tw-text-gray-700 tw-ring-1 tw-ring-gray-300 tw-ring-inset focus:tw-outline-offset-0">
+                        {{ '...' }}
+                    </span>
+
+                    <button v-if="pageNumberButtons.buttons[3]?.condition" @click="goToAnyPage(pageNumberButtons.buttons[3]?.number)" :class="[pageNumberButtons.commonClasses, (currentPage == pageNumberButtons.buttons[3]?.number) ? pageNumberButtons.currentPageClass : pageNumberButtons.notCurrentPageClass]">
+                        {{ pageNumberButtons.buttons[3]?.number }}
+                    </button>
+                    <button v-if="pageNumberButtons.buttons[4]?.condition" @click="goToAnyPage(pageNumberButtons.buttons[4]?.number)" :class="[pageNumberButtons.commonClasses, (currentPage == pageNumberButtons.buttons[4]?.number) ? pageNumberButtons.currentPageClass : pageNumberButtons.notCurrentPageClass]">
+                        {{ pageNumberButtons.buttons[4]?.number }}
+                    </button>
+                    <button v-if="pageNumberButtons.buttons[5]?.condition" @click="goToAnyPage(pageNumberButtons.buttons[5]?.number)" :class="[pageNumberButtons.commonClasses, (currentPage == pageNumberButtons.buttons[5]?.number) ? pageNumberButtons.currentPageClass : pageNumberButtons.notCurrentPageClass]">
+                        {{ pageNumberButtons.buttons[5]?.number }}
+                    </button>
                     
+                    <!-- NEXT & LAST -->
                     <button @click="nextPage" :disabled="currentPage >= totalPages" class="tw-relative tw-inline-flex tw-items-center tw-px-2 tw-py-2 tw-text-gray-400 tw-ring-1 tw-ring-gray-300 tw-ring-inset hover:tw-bg-gray-50 focus:tw-z-20 focus:tw-outline-offset-0">
                         <span class="tw-sr-only">{{ 'Siguiente' }}</span>
                         <i class="bi bi-chevron-right"></i>
@@ -80,62 +98,69 @@
     const router = useRouter();
 
     const props = defineProps({
-        // Pagination-related properties
         currentPage: {
             type: Number,
-            default: 0,
-            required: false,
+            required: true,
         },
         pageSize: {
             type: Number,
-            default: 12,
-            required: false,
+            required: true,
         },
         totalElements: {
             type: Number,
-            default: 0,
-            required: false,
+            required: true,
         },    
     });
     
     const currentPage = computed(() => props.currentPage);
     const pageSize = computed(() => props.pageSize);
-    const totalPages = computed(() => Math.ceil(props.totalElements / pageSize.value) - 1);
+    const totalPages = computed(() => Math.ceil(props.totalElements / pageSize.value));
     const totalElements = computed(() => props.totalElements);
 
-    const firstElement = computed(() => currentPage.value * pageSize.value + 1);
-    const lastElement = computed(() => (currentPage.value * pageSize.value) + pageSize.value);
-    
-    const firstPage = () => {
+    const firstElement = computed(() => ((currentPage.value - 1) * pageSize.value) + 1);
+    const lastElement = computed(() => ((currentPage.value - 1) * pageSize.value) + pageSize.value);
+
+    const pageNumberButtons = computed(() => {
+        return {
+            commonClasses: "tw-relative tw-inline-flex tw-items-center tw-px-4 tw-py-2 tw-text-sm tw-font-semibold focus:tw-z-20",
+            currentPageClass: "tw-text-white tw-z-10 tw-bg-indigo-600 focus-visible:tw-outline-2 focus-visible:tw-outline-offset-2 focus-visible:tw-outline-indigo-600",
+            notCurrentPageClass: "tw-text-gray-900 tw-ring-1 tw-ring-gray-300 tw-ring-inset hover:tw-bg-gray-50 focus:tw-outline-offset-0",
+            buttons: [
+                { condition: totalPages.value >= 1, number: (currentPage.value == 1 || currentPage.value == 2 || currentPage.value == 3) ? 1 : "fix" },
+                { condition: totalPages.value >= 2, number: (currentPage.value == 1 || currentPage.value == 2 || currentPage.value == 3) ? 2 : "fix" },
+                { condition: totalPages.value >= 3, number: (currentPage.value == 1 || currentPage.value == 2 || currentPage.value == 3) ? 3 : "fix" },
+                { condition: totalPages.value >= 4, number: (totalPages.value - 2) },
+                { condition: totalPages.value >= 5, number: (totalPages.value - 1) },
+                { condition: totalPages.value >= 6, number: totalPages.value },
+            ],
+        };
+    });
+
+    const goToAnyPage = (pageNumber) => {
         router.push({
             path: route.path,
-            query: { ...route.query, page: 0 },
+            query: { ...route.query, page: pageNumber },
         });
+    };
+
+    const firstPage = () => {
+        goToAnyPage(1);
     };
     
     const prevPage = () => {
-        if (currentPage.value > 0) {
-            router.push({
-                path: route.path,
-                query: { ...route.query, page: currentPage.value - 1 },
-            });
+        if (currentPage.value > 1) {
+            goToAnyPage(currentPage.value - 1);
         }
-    };
+    };   
 
     const nextPage = () => {
         if (currentPage.value < totalPages.value) {
-            router.push({
-                path: route.path,
-                query: { ...route.query, page: currentPage.value + 1 },
-            });
+            goToAnyPage(currentPage.value + 1);
         }
     };
 
     const lastPage = () => {
-        router.push({
-            path: route.path,
-            query: { ...route.query, page: totalPages.value },
-        });
+        goToAnyPage(totalPages.value);
     };
     
 </script>
