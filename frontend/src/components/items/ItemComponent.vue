ebdb8bdb550ace2949858eddd06c243ffdd8cef7
pblvicente <pvicentebusiness@gmail.com>
2025-04-04 12:21:48 +0200

    [MOD] Quitado todo el código que pedía a la API la información del producto a través de la máscara en ItemComponent, ya que ahora se le pasa como prop el objeto del producto con toda la información

--- frontend/src/components/items/ItemComponent.vue
+++ frontend/src/components/items/ItemComponent.vue
@@ -1,6 +1,5 @@
 <template>
 
-    <!-- SUCCESS -->
     <ElementComponent v-if="!loading && !error" :toRoute="{ name: 'product', params: { mask: item.id } }" :imgSrc="apiBaseURL + `/images/` + item.mask + `/image.png`" :imgAlt="`${item.mask}`">
         <template #bottom-right>
             <CartActionsButtonComponent :item="item" :class="'m-1'"/>
@@ -18,74 +17,21 @@
         </template>        
     </ElementComponent>
 
-    <!-- LOADING -->
-    <ElementComponent v-if="loading">
-        <template #middle-center>
-            <div class="spinner-border">
-                <span class="visually-hidden">{{ 'Loading...' }}</span>
-            </div>
-        </template>
-        <template #footer>
-            <div class="col text-truncate placeholder-glow">
-                <span class="placeholder">{{ 'Disponibles: ?' }}</span>
-            </div>
-            <div class="col-12 col-md-auto placeholder-glow">
-                <span class="placeholder">{{ '??????' }}</span>
-            </div>
-        </template>      
-    </ElementComponent>
-
-    <!-- ERROR -->
-    <ElementComponent v-if="error">
-        <template #middle-center>
-            <i class="bi bi-exclamation-diamond-fill tw-text-red-500 fs-1"></i>
-        </template>
-        <template #footer>
-            <div class="col text-truncate placeholder-glow">
-                <span class="placeholder">{{ 'Disponibles: ?' }}</span>
-            </div>
-            <div class="col-12 col-md-auto placeholder-glow">
-                <span class="placeholder">{{ '??????' }}</span>
-            </div>
-        </template>      
-    </ElementComponent>
-
 </template>
 
 <script setup>
 
-    import { ref, onMounted } from 'vue';
+    import { ref } from 'vue';
     import apiClient from '../../services/api.js';
 
     const apiBaseURL = ref(apiClient.getApiBaseURL());
 
     const props = defineProps({
-        itemMask: {
-            type: Number,
-            required: true,
-        },
-    });
-
-    const item = ref([]);
-    const loading = ref(true);
-    const error = ref(null);
-
-    const fetchItem = async () => {
-
-        const response = await apiClient.getItem(props.itemMask)
-
-        if (response.success) {
-            item.value = response.data;
-        } else {
-            error.value = response.error;
+        item: {
+            type: Object,
+            default: [],
+            required: true
         }
-
-    };
-
-    onMounted(async () => {
-        loading.value = true;
-        await fetchItem();
-        loading.value = false;
     });
 
 </script>
