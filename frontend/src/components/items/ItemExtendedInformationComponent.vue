86baee08f92243bd6ece248aae7c1e17fb4ddf24
pblvicente <pvicentebusiness@gmail.com>
2025-04-04 14:50:58 +0200

    [MOD] Cambiado img por ImageExtensionCheckerComponent y cambiado el elemento que se muestra mientras se carga en ItemExtendedInformationComponent

--- frontend/src/components/items/ItemExtendedInformationComponent.vue
+++ frontend/src/components/items/ItemExtendedInformationComponent.vue
@@ -5,7 +5,7 @@
 
       <div class="col-12 col-lg-4 tw-bg-cyan-500/10">
           <div class="ratio ratio-1x1">
-            <img class="object-fit-contain p-1" :src="'/watch_back.avif'" :alt="item.name">
+            <ImageExtensionCheckerComponent :class="`object-fit-contain p-5`" :src="apiBaseURL + `/images/` + item.mask + `/image.png`" :alt="item.name"/>
           </div>          
       </div>
 
@@ -14,7 +14,7 @@
           <div class="tw-font-semibold tw-text-2xl">{{ item.name }}</div>
           <div class="tw-font-semibold tw-text-lg">{{ 'Precio: ' + item.price.toFixed(2) + '€'}}</div>
           <div class="tw-font-semibold tw-text-lg">{{ 'Disponibles: ' + item.stock }}</div>
-          <div class="tw-font-semibold tw-text-lg">{{ 'Marca: ' + item.brand_name }}</div>
+          <div class="tw-font-semibold tw-text-lg">{{ 'Marca: ' + item.brand_id }}</div>
         </div>          
       </div>
 
@@ -28,8 +28,26 @@
   </div>
 
   <!-- LOADING -->
-  <div v-if="loading" class="tw-m-4 sm:tw-m-0">
-    <WordsLoaderComponent :mainWords="`cargando...`" :secondaryWords="['nombre', 'descripción', 'precio', 'existencias', 'marca']"/>
+  <div v-if="loading" class="row overflow-hidden sm:tw-border-2 sm:tw-rounded-lg">
+
+      <div class="col-12 col-lg-4 tw-bg-cyan-500/10">
+          <div class="ratio ratio-1x1">
+            <ImageExtensionCheckerComponent :class="`object-fit-contain`" :src="'/no_image.png'" :alt="item.name"/>
+          </div>          
+      </div>
+
+      <div class="col-12 col-lg-8 tw-bg-cyan-500/25">
+        <div class="tw-grid tw-content-center tw-justify-start tw-space-y-2 tw-p-4 tw-h-full">
+          <WordsLoaderComponent :mainWords="`cargando...`" :secondaryWords="['nombre', 'precio', 'existencias', 'marca', 'nombre']"/>
+        </div>          
+      </div>
+
+      <div class="col-12 tw-bg-cyan-500/35">
+        <div class="tw-space-y-2 tw-p-4">
+          <WordsLoaderComponent :mainWords="`cargando...`" :secondaryWords="['descripción', 'descripción', 'descripción', 'descripción', 'descripción']"/>
+        </div>         
+      </div>
+
   </div>
 
   <!-- ERROR -->
@@ -45,67 +63,42 @@
     import { ref, onMounted, defineProps } from 'vue';
     import apiClient from '../../services/api.js';
 
+    const apiBaseURL = ref(apiClient.getApiBaseURL());
+
     const props = defineProps({
         itemMask: {
-            type: String,
-            default: '',
+            type: Number,
+            default: 0,
+            required: true
         },
     });
 
-    const item = ref([]);
+    const item = ref({});
     const loading = ref(true);
     const error = ref(null); 
 
-    // const fetchItem = async () => {
-    //     try {
-    //         const response = await apiClient.getItem(props.itemMask);
-    //         item.value = response.data;
-    //     } catch (err) {
-    //         error.value = 'Error al cargar el producto';
-    //     }
-    // };
+    const fetchItem = async () => {
 
-    // onMounted(async () => {
-    //     loading.value = true;
-    //     await Promise.all([fetchItem()]);
-    //     loading.value = false;
-    // });
+      loading.value = true;
 
-    onMounted(async () => {
-        loading.value = true;
-        setTimeout(() => {
-            loading.value = false;        
+      const response = await apiClient.getItem(props.itemMask);
 
-            if(props.itemMask == "audemars-piguet-26470st-royal-oak-offshore"){
+      if (response.success) {
 
-                item.value = {
-                name: '26470ST Royal Oak Offshore',
-                description: 'Emblemático Royal Oak Offshore con caja de acero inoxidable de 42mm, cronógrafo automático y esfera "Méga Tapisserie". Resistente al agua hasta 100m y brazalete integrado de acero.',
-                price: 34500.00,
-                stock: 3,
-                mask: 'audemars-piguet-26470st-royal-oak-offshore',
-                brand_id: 1,
-                brand_name: 'Audemars Piguet'
-                };
+        item.value = response.data;
 
-            } else if(props.itemMask == "hermes-ha3210-harnais"){
+      } else {
 
-                item.value = {
-                name: 'HA3.210 Harnais',
-                description: 'Harnais con diseño inspirado en los arneses ecuestres, esfera guilloché y movimiento de precisión',
-                price: 3950.00,
-                stock: 4,
-                mask: 'hermes-ha3210-harnais',
-                brand_id: 4,
-                brand_name: 'Hermès'
-                };
+        error.value = response.error;
+        
+      }
 
-            } else {
+      setTimeout(() => {
+        loading.value = false;
+      }, 2500);
 
-                error.value = 'Error al cargar el producto';
+    };
 
-            }
-      }, 4000);
-    });
+    onMounted(fetchItem);
 
 </script>
