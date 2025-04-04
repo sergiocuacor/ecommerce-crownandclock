4907b5d2910595c65d41ec7b9073cca50fe76140
pblvicente <pvicentebusiness@gmail.com>
2025-04-04 12:31:01 +0200

    [MOD] Reestructurado BrandsView

--- frontend/src/views/BrandsView.vue
+++ frontend/src/views/BrandsView.vue
@@ -1,11 +1,13 @@
 <template>
-    <main>
-        <VideoBackgroundComponent :videoSrc="'/video1.mp4'">
-            <template #main>
-                <p class="tw-font-semibold tw-subpixel-antialiased tw-font-mono tw-text-8xl tw-tracking-widest tw-text-white">{{ 'MARCAS' }}</p>
-            </template>
-        </VideoBackgroundComponent>
 
-        <BrandsComponent />
+    <VideoBackgroundComponent :videoSrc="'/video1.mp4'">
+        <template #main>
+            <p class="tw-font-semibold tw-subpixel-antialiased tw-font-mono tw-text-8xl tw-tracking-widest tw-text-white">{{ 'MARCAS' }}</p>
+        </template>
+    </VideoBackgroundComponent>
+
+    <main class="container py-3">
+        <BrandsComponent/>
     </main>
+    
 </template>
