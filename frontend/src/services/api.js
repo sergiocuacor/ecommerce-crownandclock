838c3ab6c68a8dd9f7354cc5223ce7d3f1e3ae46
pblvicente <pvicentebusiness@gmail.com>
2025-04-06 13:05:38 +0200

    [MOD] Añadido el endpoint para sacar la marca por id en la api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,39 +1,5 @@
 import axios from 'axios';
 
-// const apiClient = axios.create({
-//   baseURL: 'https://fakestoreapi.com',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-// });
-
-// export default {
-//   getItem(mask) {
-//     return apiClient.get(`/products/${mask}`);
-//   },
-//   getItems() {
-//     return apiClient.get('/products');
-//   },
-//   getBrand(mask) {
-//     return apiClient.get(`/brands/${mask}`);
-//   },
-//   getBrands() {
-//     return apiClient.get('/brands');
-//   },
-//   getCategory(mask) {
-//     return apiClient.get(`/categories/${mask}`);
-//   },
-//   getCategories() {
-//     return apiClient.get('/products/categories');
-//   },
-//   getItemsFromBrand(mask) {
-//     return apiClient.get(`/products/brand/${mask}`);
-//   }, 
-//   getItemsFromCategory(mask) {
-//     return apiClient.get(`/products/category/${mask}`);
-//   },  
-// };
-
 const apiClient = axios.create({
   baseURL: 'http://localhost:8080',
   headers: {
@@ -63,8 +29,8 @@ export default {
   getItemsPageable(page = 0, size = 12, sort = 'name,asc') {
     return apiCall('get', '/product', { page, size, sort });
   },
-  getItemsFromBrand(id) {
-    return apiCall('get', `/brand/${id}`)
+  getBrandById(id) {
+    return apiCall('get', `/brand/${id}`);
   },
   getBrands() {
     return apiCall('get', '/brand');
