303d67d2ac6ad91bddf840af2dfa293ca093c824
pblvicente <pvicentebusiness@gmail.com>
2025-04-17 17:53:52 +0200

    [ADD] Creada la vista RegisterView

---
+++ frontend/src/views/RegisterView.vue
@@ -0,0 +1,11 @@
+<template>
+
+    <main>
+        <VideoBackgroundComponent :videoSrc="'/video1.mp4'">
+            <template #main>
+                <RegisterFormComponent />
+            </template>
+        </VideoBackgroundComponent>
+    </main>
+
+</template>
