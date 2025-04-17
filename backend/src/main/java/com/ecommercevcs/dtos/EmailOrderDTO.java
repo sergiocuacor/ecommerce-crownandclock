918abfc04a90932ae64ec0e727910cea226f51e0
carloss <carlosrose96@gmail.com>
2025-04-17 18:42:26 +0200

    [MOD] modificando los estilos de los html

--- backend/src/main/java/com/ecommercevcs/dtos/EmailOrderDTO.java
+++ backend/src/main/java/com/ecommercevcs/dtos/EmailOrderDTO.java
@@ -1,46 +1,41 @@
 package com.ecommercevcs.dtos;
 
 public class EmailOrderDTO {
-	
+
 	public String name;
 	public String mask;
 	public Integer quantity;
-	
-	
+
 	public EmailOrderDTO(String name, String mask, Integer quantity) {
 		super();
 		this.name = name;
 		this.mask = mask;
 		this.quantity = quantity;
-		
-	}
-	
-	
-	
 
+	}
 
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getMask() {
 		return mask;
 	}
+
 	public void setMask(String mask) {
 		this.mask = mask;
 	}
+
 	public Integer getQuantity() {
 		return quantity;
 	}
+
 	public void setQuantity(Integer quantity) {
 		this.quantity = quantity;
 	}
 
-	
-	
-	
-	
-	
 }
