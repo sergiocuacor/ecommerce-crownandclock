8e5e1b70799c7a41840fae4bff738dcbe3b9662d
pblvicente <pvicentebusiness@gmail.com>
2025-04-19 22:30:34 +0200

    [MOD] Mejorado el código y el diseño de CartActionsButtonComponent

--- frontend/src/components/cart/CartActionsButtonComponent.vue
+++ frontend/src/components/cart/CartActionsButtonComponent.vue
@@ -1,20 +1,38 @@
 <template>
-    <div class="btn-group">
-        <button class="btn btn-sm btn-success" @click="addToCart" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar">
+
+    <div class="tw-bg-gray-500/25 tw-flex tw-p-0.5 tw-rounded-md">
+
+        <button
+            class="tw-bg-white tw-text-black tw-rounded-s-md tw-w-12 tw-h-12 sm:tw-w-10 sm:tw-h-10 tw-flex tw-items-center tw-justify-center tw-shadow-md hover:tw-bg-gray-100 disabled:tw-opacity-50"
+            @click="addToCart"
+            :disabled="isDisabledAdd"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+        >
             <i class="bi bi-bag-plus-fill"></i>
         </button>
-        <button type="button" class="btn btn-sm btn-danger" @click="removeFromCart" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" :disabled="isDisabled">
+    
+        <button
+            type="button"
+            class="tw-bg-black tw-text-white tw-rounded-e-md tw-w-12 tw-h-12 sm:tw-w-10 sm:tw-h-10 tw-flex tw-items-center tw-justify-center tw-shadow-md hover:tw-bg-gray-800 disabled:tw-opacity-50"
+            @click="removeFromCart"
+            data-bs-toggle="offcanvas"
+            data-bs-target="#offcanvasNavbar"
+            :disabled="isDisabledRemove"
+        >
             <i class="bi bi-bag-dash-fill"></i>
-        </button>       
-    </div>  
-</template>
+        </button>
+
+    </div>
+
+</template>  
 
 <script setup>
 
-    import { ref, onMounted } from 'vue';
+    import { computed } from 'vue';
     import { useCartStore } from '../../store/cart.js';
     import { useUtilsStore } from '../../services/utils.js';
-    
+
     const props = defineProps({
         item: {
             type: Object,
@@ -25,22 +43,23 @@
     const cartStore = useCartStore();
     const utilsStore = useUtilsStore();
 
-    const isDisabled = ref(true);
+    const isDisabledRemove = computed(() => {
+        return !cartStore.items.some(item => item.id === props.item.id);
+    });
+
+    const isDisabledAdd = computed(() => {
+        const itemInCart = cartStore.items.find(item => item.id === props.item.id);
+        return itemInCart ? itemInCart.quantity >= itemInCart.stock : false;
+    });
 
     const addToCart = () => {
         cartStore.addToCart(props.item);
         utilsStore.setOffCanvasContent('cart');
-        isDisabled.value = false;
     };
 
     const removeFromCart = () => {
         cartStore.removeFromCart(props.item.id);
         utilsStore.setOffCanvasContent('cart');
-        isDisabled.value = true;
     };
-
-    onMounted(() => {
-        isDisabled.value = !cartStore.items.some(item => item.id === props.item.id);
-    });
 
 </script>
