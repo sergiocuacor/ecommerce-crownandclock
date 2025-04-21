4a7966972aec5b3240fce4f4b2cdbd0c7ea8293b
Sergio Cuadrado <2sergiocuadrado2000@gmail.com>
2025-04-21 17:59:21 +0200

    [ADD] Reports - ReportService: generacion excel sheet de ventas diarias

--- backend/src/main/java/com/ecommercevcs/repositories/OrderRepository.java
+++ backend/src/main/java/com/ecommercevcs/repositories/OrderRepository.java
@@ -1,12 +1,14 @@
 package com.ecommercevcs.repositories;
 
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.ecommercevcs.dtos.DiscountDTO;
+
 import com.ecommercevcs.entities.OrderEntity;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, Long>{
@@ -15,7 +17,7 @@ public interface OrderRepository extends JpaRepository<OrderEntity, Long>{
 	
 	@Query("SELECT EXISTS (SELECT 1 FROM OrderEntity o JOIN o.orderDetails od " +
 		       "WHERE o.user.id = :userId AND od.product.id = :productId)")
-		boolean existsByUserIdAndProductId(Long userId, Long productId);
+	boolean existsByUserIdAndProductId(Long userId, Long productId);
 	
-	
+	List<OrderEntity> findByOrderDate(LocalDateTime startDate, LocalDateTime endDate);
 }
