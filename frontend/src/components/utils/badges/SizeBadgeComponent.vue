b3677df5bb836f81f8f2dfb90e0de1de595fd81b
pblvicente <pvicentebusiness@gmail.com>
2025-04-26 13:37:28 +0200

    [ADD] Creado SizeBadgeComponent para usarlo en otros componentes

---
+++ frontend/src/components/utils/badges/SizeBadgeComponent.vue
@@ -0,0 +1,19 @@
+<template>
+
+    <span class="tw-bg-black tw-text-white tw-py-0.5 tw-px-3 tw-rounded-full tw-font-semibold tw-text-sm">
+        {{ 'Watches per page: ' + props.pageSize }}
+    </span>
+
+</template>
+
+<script setup>
+
+    import { defineProps } from 'vue';
+
+    const props = defineProps({
+        pageSize: {
+            required: true,
+        },
+    });
+
+</script>
