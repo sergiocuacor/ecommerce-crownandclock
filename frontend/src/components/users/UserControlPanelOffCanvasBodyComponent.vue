17a54d37bf8ab659a8d2eef209e6ed958bece0ed
pblvicente <pvicentebusiness@gmail.com>
2025-04-29 23:09:16 +0200

    [MOD] UserControlPanelOffCanvasBodyComponent modificado con todas las rutas

--- frontend/src/components/users/UserControlPanelOffCanvasBodyComponent.vue
+++ frontend/src/components/users/UserControlPanelOffCanvasBodyComponent.vue
@@ -12,13 +12,19 @@
             <div class="collapse show" id="home-collapse">
                 <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
                     <li>
-                        <a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Overview</a>
+                        <router-link :to="{ name: 'home' }" class="link-body-emphasis d-inline-flex text-decoration-none rounded">
+                            {{ 'Home' }}
+                        </router-link>
                     </li>
                     <li>
-                        <a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Updates</a>
+                        <router-link :to="{ name: 'products' }" class="link-body-emphasis d-inline-flex text-decoration-none rounded">
+                            {{ 'Watches' }}
+                        </router-link>
                     </li>
                     <li>
-                        <a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Reports</a>
+                        <router-link :to="{ name: 'brands' }" class="link-body-emphasis d-inline-flex text-decoration-none rounded">
+                            {{ 'Brands' }}
+                        </router-link>
                     </li>
                 </ul>
             </div>
@@ -26,21 +32,16 @@
         <li class="mb-1">
             <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#dashboard-collapse" aria-expanded="false">
                 <i class="bi bi-square-half fs-6 fw-semibold me-1"></i>
-                <span class="fs-6 fw-semibold">Dashboard</span>
+                <span class="fs-6 fw-semibold">
+                    {{ 'Dashboard' }}
+                </span>
             </button>
             <div class="collapse" id="dashboard-collapse">
                 <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
                     <li>
-                        <a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Overview</a>
-                    </li>
-                    <li>
-                        <a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Weekly</a>
-                    </li>
-                    <li>
-                        <a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Monthly</a>
-                    </li>
-                    <li>
-                        <a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Annually</a>
+                        <router-link :to="{ name: 'reviews' }" class="link-body-emphasis d-inline-flex text-decoration-none rounded">
+                            {{ 'Reviews' }}
+                        </router-link>
                     </li>
                 </ul>
             </div>
@@ -48,33 +49,36 @@
         <li class="mb-1">
             <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#orders-collapse" aria-expanded="false">
                 <i class="bi bi-box-seam-fill fs-6 fw-semibold me-1"></i>
-                <span class="fs-6 fw-semibold">Orders</span>
+                <span class="fs-6 fw-semibold">
+                    {{ 'Orders' }}
+                </span>
             </button>
             <div class="collapse" id="orders-collapse">
                 <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
                     <li>
-                        <a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">New</a></li>
-                    <li>
-                        <a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Processed</a>
+                        <router-link :to="{ name: 'orders' }" class="link-body-emphasis d-inline-flex text-decoration-none rounded">
+                            {{ 'All' }}
+                        </router-link>
                     </li>
                     <li>
-                        <a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Shipped</a>
+                        <router-link :to="{ name: 'orders', params: { mask: 'completed' } }" class="link-body-emphasis d-inline-flex text-decoration-none rounded">
+                            {{ 'Completed' }}
+                        </router-link>
                     </li>
                     <li>
-                        <a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Returned</a>
+                        <router-link :to="{ name: 'orders', params: { mask: 'shipped' } }" class="link-body-emphasis d-inline-flex text-decoration-none rounded">
+                            {{ 'Shipped' }}
+                        </router-link>
                     </li>
-                </ul>
-            </div>
-        </li>
-        <li class="mb-1">
-            <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#products-collapse" aria-expanded="false">
-                <i class="bi bi-boxes fs-6 fw-semibold me-1"></i>
-                <span class="fs-6 fw-semibold">Products</span>
-            </button>
-            <div class="collapse" id="products-collapse">
-                <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
                     <li>
-                        <a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Wishlist</a>
+                        <router-link :to="{ name: 'orders', params: { mask: 'pending' } }" class="link-body-emphasis d-inline-flex text-decoration-none rounded">
+                            {{ 'Pending' }}
+                        </router-link>
+                    </li>
+                    <li>
+                        <router-link :to="{ name: 'orders', params: { mask: 'cancelled' } }" class="link-body-emphasis d-inline-flex text-decoration-none rounded">
+                            {{ 'Cancelled' }}
+                        </router-link>
                     </li>
                 </ul>
             </div>
@@ -83,26 +87,39 @@
         <li class="mb-1">
             <button class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" data-bs-target="#account-collapse" aria-expanded="false">
                 <i class="bi bi-person-circle fs-6 fw-semibold me-1"></i>
-                <span class="fs-6 fw-semibold">Account</span>
+                <span class="fs-6 fw-semibold">
+                    {{ 'Account' }}
+                </span>
             </button>
             <div class="collapse" id="account-collapse">
                 <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">
-                    <li>
-                        <a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">New...</a>
-                    </li>
                     <li>
                         <router-link :to="{ name: 'profile' }" class="link-body-emphasis d-inline-flex text-decoration-none rounded">
                             {{ 'Profile' }}
                         </router-link>
                     </li>
                     <li>
-                        <a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Settings</a>
-                    </li>
-                    <li>
-                        <a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded">Sign out</a>
+                        <a href="" @click="logOut" class="link-body-emphasis d-inline-flex text-decoration-none rounded">
+                            {{ 'Sign out' }}
+                        </a>
                     </li>
                 </ul>
             </div>
         </li>
     </ul>
 </template>
+
+<script setup>
+
+    import { useAuthStore } from '../../services/auth.js';   
+    import { useRouter } from 'vue-router';
+
+    const authStore = useAuthStore();
+    const router = useRouter();    
+
+    function logOut() {
+        authStore.logOutUser();
+        router.push('/login');
+    }
+
+</script>
