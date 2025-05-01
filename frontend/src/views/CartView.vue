04b131de8e2458bd09700e99c4ca035b72a7991e
pblvicente <pvicentebusiness@gmail.com>
2025-05-01 14:16:05 +0200

    [MOD] Cambiados los mensajes para cuando el carrito esta vacío en CartViewComponent

--- frontend/src/views/CartView.vue
+++ frontend/src/views/CartView.vue
@@ -3,7 +3,7 @@
   <!-- FULL -->
   <main v-if="cartStore.items.length > 0" class="container tw-pt-20 tw-pb-2.5">
     <div class="tw-text-2xl tw-text-center tw-p-5 tw-bg-gray-300 tw-border-2 tw-rounded-t-lg">
-      {{ 'Carrito' }}
+      {{ 'Cart' }}
     </div>
     <CartItemsComponent />
     <CartCheckoutComponent class="tw-pt-2.5"/>
@@ -13,12 +13,12 @@
   <DynamicHeightDivComponent v-else>
     <template #header>
         <div class="tw-font-mono tw-text-md lg:tw-text-xl tw-bg-gray-200 tw-px-2 tw-rounded">
-            {{ 'Carrito Vacío' }}
+            {{ 'Empty cart' }}
         </div>
     </template>
     <template #footer >
         <div class="tw-font-mono tw-text-xs lg:tw-text-base">
-            {{ 'No hay productos' }}
+            {{ 'No watches added' }}
         </div>
     </template>
   </DynamicHeightDivComponent>
