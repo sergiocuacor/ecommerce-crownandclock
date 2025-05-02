53a9db542441274a49a5f038acfc1963ee6b1f4a
pblvicente <pvicentebusiness@gmail.com>
2025-05-02 18:04:49 +0200

    [FIX] Ahora muestra correctamente los nombres en SortBadgeComponent

--- frontend/src/components/utils/badges/SortBadgeComponent.vue
+++ frontend/src/components/utils/badges/SortBadgeComponent.vue
@@ -36,49 +36,49 @@
         if (!labels[props.sortBy]) {
             switch (props.sortBy) {
                 case 'name':
-                    updateLabels('name,asc', 'Name (A-Z)');
+                    updateLabels(props.sortBy, 'Name (A-Z)');
                     break;
                 case 'price':
-                    updateLabels('price,asc', 'Price (Low to High)');
+                    updateLabels(props.sortBy, 'Price (Low to High)');
                     break;
                 case 'brand':
-                    updateLabels('brand,asc', 'Brand (A-Z)');
+                    updateLabels(props.sortBy, 'Brand (A-Z)');
                     break;
                 case 'stock':
-                    updateLabels('stock,asc', 'Stock (Low to High)');
+                    updateLabels(props.sortBy, 'Stock (Low to High)');
                     break;
                 case 'mask':
-                    updateLabels('mask', 'Mask (A-Z)');
+                    updateLabels(props.sortBy, 'Mask (A-Z)');
                     break;
                 case 'mask,asc':
-                    updateLabels('mask,asc', 'Mask (A-Z)');
+                    updateLabels(props.sortBy, 'Mask (A-Z)');
                     break;
                 case 'mask,desc':
-                    updateLabels('mask,desc', 'Mask (Z-A)');
+                    updateLabels(props.sortBy, 'Mask (Z-A)');
                     break;
                 case 'description':
-                    updateLabels('description', 'Description (A-Z)');
+                    updateLabels(props.sortBy, 'Description (A-Z)');
                     break;
                 case 'description,asc':
-                    updateLabels('description,asc', 'Description (A-Z)');
+                    updateLabels(props.sortBy, 'Description (A-Z)');
                     break;
                 case 'description,desc':
-                    updateLabels('description,desc', 'Description (Z-A)');
+                    updateLabels(props.sortBy, 'Description (Z-A)');
                     break;
                 case 'brand_id':
-                    updateLabels('brand_id', 'Brand ID (A-Z)');
+                    updateLabels(props.sortBy, 'Brand ID (A-Z)');
                     break;
                 case 'brand_id,asc':
-                    updateLabels('brand_id,asc', 'Brand ID (A-Z)');
+                    updateLabels(props.sortBy, 'Brand ID (A-Z)');
                     break;
                 case 'brand_id,desc':
-                    updateLabels('brand_id,desc', 'Brand ID (Z-A)');
+                    updateLabels(props.sortBy, 'Brand ID (Z-A)');
                     break;
                 default:
                     updateLabels(props.sortBy, 'Unknown');
             }
         }
-        return labels;
+        return labels[props.sortBy];
     });
     
 </script>
