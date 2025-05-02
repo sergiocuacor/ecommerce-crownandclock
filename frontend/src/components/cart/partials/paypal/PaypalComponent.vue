ae343277440f1fad379b60a3e08277e5208264e3
pblvicente <pvicentebusiness@gmail.com>
2025-05-02 02:07:25 +0200

    [ADD] Creado PaypalComponent para simular pagar con Paypal

---
+++ frontend/src/components/cart/partials/paypal/PaypalComponent.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <div class="tw-grid tw-grid-cols-12 tw-gap-5 tw-p-5 tw-bg-gray-400/25 tw-rounded-[10px]">
+
+        <div class="tw-col-span-12 tw-flex tw-justify-center tw-items-center">
+            <PaypalInformationFormComponent @validatedPaypalInformation="validatePaymentMethod"/>
+        </div>
+
+    </div>
+
+</template>
+
+<script setup>
+
+    import { defineEmits, ref } from 'vue';
+
+    const emit = defineEmits(['validatedPaymentMethod']);
+
+    const creditCardInformation = ref({
+        holderName: null,
+        number: null,       
+        expiryDate: null,
+        cvv: null,
+    });
+
+    const validatePaymentMethod = (isValid) => {
+        emit('validatedPaymentMethod', isValid);
+    };
+
+</script>
