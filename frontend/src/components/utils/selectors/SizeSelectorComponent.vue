dbdc92896360e67ae287476e44021f57f8bcdd6e
pblvicente <pvicentebusiness@gmail.com>
2025-05-02 17:32:57 +0200

    [MOD] Quitado código innecesario de SizeSelectorComponent

--- frontend/src/components/utils/selectors/SizeSelectorComponent.vue
+++ frontend/src/components/utils/selectors/SizeSelectorComponent.vue
@@ -1,9 +1,14 @@
 <template>
 
-    <label for="sizeSelector" class="tw-block tw-text-md tw-font-medium tw-text-gray-700 tw-py-2">{{ 'Watches per page: ' }}</label>
+    <label for="sizeSelector" class="tw-block tw-text-md tw-font-medium tw-text-gray-700 tw-py-2">
+        {{ 'Watches per page: ' }}
+    </label>
+    
     <select name="sizeSelector" id="sizeSelector" class="tw-block tw-w-full tw-border tw-border-gray-300 tw-rounded-md tw-py-2 tw-px-3 tw-shadow-md focus:tw-border-gray-400 focus:tw-ring-gray-400 tw-text-md" v-model="selectedSize" @change="handleChange">
 
-        <option v-for="size in sizes" :key="size" :value="size" :disabled="size == selectedSize">{{ size }}</option>
+        <option v-for="size in sizes" :key="size" :value="size" :disabled="size == selectedSize">
+            {{ size }}
+        </option>
 
     </select>
 
@@ -17,7 +22,6 @@
     const route = useRoute();
     const router = useRouter();
 
-    const emit = defineEmits(['sizeSelected']);
     const props = defineProps({
         pageSize: {
             type: Number,
