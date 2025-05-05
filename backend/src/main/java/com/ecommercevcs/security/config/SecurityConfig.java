3c3fa0d222d06da06937f2f3ce0b4ca7c5f19515
pblvicente <pvicentebusiness@gmail.com>
2025-05-05 21:07:17 +0200

    [MOD] SecurityConfig, mi primera aportación al backend

--- backend/src/main/java/com/ecommercevcs/security/config/SecurityConfig.java
+++ backend/src/main/java/com/ecommercevcs/security/config/SecurityConfig.java
@@ -46,33 +46,85 @@ public class SecurityConfig {
 
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
-	    return http
-	        .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF si usas JWT
-	       .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CONFIGURACIÓN DEL CORS, TODOS LOS MÉTODOS DE ABAJO
-	        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Sin sesiones
-	        .authorizeHttpRequests(auth -> auth
-	            .requestMatchers(
-	            	"/**",
-	                "/users", 
-	                "/swagger-ui/**", 
-	                "/v3/api-docs/**",   // Documentación OpenAPI
-	                "/swagger-resources/**", 
-	                "/webjars/**"         // Recursos estáticos de Swagger
-	            ).permitAll() // Endpoints públicos
-	            //.requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
-//	            .requestMatchers(HttpMethod.POST, "/brand").hasRole("ADMIN") SOLO PUEDE CREAR BRANDS ADMIN
-	           // .requestMatchers(HttpMethod.POST, "/brand/product/{id}").hasRole("ADMIN") NO VALE
-	           // .requestMatchers(HttpMethod.PUT, "/brand").hasRole("ADMIN") 
-	         // .requestMatchers(HttpMethod.PUT, "/product").hasRole("ADMIN") 
-	          //  .requestMatchers(HttpMethod.PUT, "/product").hasRole("ADMIN")
-	          //  .requestMatchers(HttpMethod.DELETE, "/product/{id}").hasRole("ADMIN")
-	        //  .requestMatchers(HttpMethod.DELETE, "/brand/{id}").hasRole("ADMIN")
-	        //    .anyRequest().authenticated() // Todo lo demás requiere autenticación
-	        )
-	        .addFilter(new JWTAuthenticationFilter(authenticationManager)) // Agregar el filtro de autenticación JWT
-	        .addFilter(new JWTValidationFilter(authenticationManager))
-	        .build();
-	    
+    return http
+        .csrf(csrf -> csrf.disable()) // Deshabilitar CSRF si usas JWT
+       	// .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // Sin sesiones
+        .authorizeHttpRequests(auth -> auth
+				//Recursos media
+				.requestMatchers("/images/**").permitAll()
+
+                // Endpoints públicos para BrandController
+                .requestMatchers(HttpMethod.GET, "/brands/all").permitAll()
+                .requestMatchers(HttpMethod.GET, "/brands/{brandId}").permitAll()
+                .requestMatchers(HttpMethod.GET, "/brands/detailed/all").permitAll()
+                .requestMatchers(HttpMethod.GET, "/brands/detailed/{id}").permitAll()
+                .requestMatchers(HttpMethod.GET, "/brands/{brandName}/products").permitAll()
+                
+                // Endpoints públicos para ProductController
+                .requestMatchers(HttpMethod.GET, "/products").permitAll()
+                .requestMatchers(HttpMethod.GET, "/products/all").permitAll()
+                .requestMatchers(HttpMethod.GET, "/products/{id}").permitAll()
+                .requestMatchers(HttpMethod.GET, "/products/mask/{mask}").permitAll()
+                .requestMatchers(HttpMethod.GET, "/products/stock/{productId}").permitAll()
+                
+                // Endpoints públicos para ReviewController
+                .requestMatchers(HttpMethod.GET, "/reviews/product/{productId}").permitAll()
+                
+                // Endpoint público para registro de usuarios
+                .requestMatchers(HttpMethod.POST, "/users/register").permitAll()
+                
+                // Endpoints que requieren rol ADMIN para BrandController
+                .requestMatchers(HttpMethod.POST, "/brands").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/brands/{id}").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/brands/{id}").hasRole("ADMIN")
+                
+                // Endpoints que requieren rol ADMIN para ProductController
+                .requestMatchers(HttpMethod.DELETE, "/products/{id}").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/products/{id}").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "/products/{brandId}").hasRole("ADMIN")
+                
+                // Endpoints que requieren ADMIN para OrderController
+                .requestMatchers(HttpMethod.GET, "/orders/all").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "/orders/{id}").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "/orders/{id}").hasRole("ADMIN")
+                
+                // Endpoint que requiere ADMIN para UserController
+                .requestMatchers(HttpMethod.GET, "/users/all").hasRole("ADMIN")
+                
+                // Endpoints que requieren autenticación (ADMIN o USER) para OrderController
+                .requestMatchers(HttpMethod.GET, "/orders/{id}").hasAnyRole("ADMIN", "USER")
+                .requestMatchers(HttpMethod.POST, "/orders").hasAnyRole("ADMIN", "USER")
+                .requestMatchers(HttpMethod.GET, "/orders/discounts/{userId}").hasAnyRole("ADMIN", "USER")
+                
+                // Endpoints que requieren autenticación (ADMIN o USER) para UserController
+                .requestMatchers(HttpMethod.GET, "/users/{id}").hasAnyRole("ADMIN", "USER")
+                .requestMatchers(HttpMethod.PUT, "/users/{id}").hasAnyRole("ADMIN", "USER")
+                .requestMatchers(HttpMethod.DELETE, "/users/{id}").hasAnyRole("ADMIN", "USER")
+                .requestMatchers(HttpMethod.GET, "/users/me").hasAnyRole("ADMIN", "USER")
+                
+                // Endpoints que requieren autenticación (ADMIN o USER) para ReviewController
+                .requestMatchers(HttpMethod.POST, "/reviews/{userId}/{productId}").hasAnyRole("ADMIN", "USER")
+                .requestMatchers(HttpMethod.GET, "/reviews/user/{userId}").hasAnyRole("ADMIN", "USER")
+                .requestMatchers(HttpMethod.PUT, "/reviews/{reviewId}/user/{userId}").hasAnyRole("ADMIN", "USER")
+                .requestMatchers(HttpMethod.DELETE, "/reviews/{reviewId}/user/{userId}").hasAnyRole("ADMIN", "USER")
+                
+                // Endpoints para generar informes de venta, accesibles únicamente para ADMIN
+                .requestMatchers(HttpMethod.GET, "/reports/monthly-sales/pdf").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/reports/monthly-sales/pdf/{year}/{month}").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/reports/monthly-sales/excel").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/reports/monthly-sales/excel/{year}/{month}").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/reports/daily-sales/pdf").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/reports/daily-sales/pdf/{date}").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/reports/daily-sales/excel").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.GET, "/reports/daily-sales/excel/{date}").hasRole("ADMIN")
+                
+                // Por defecto, cualquier otra solicitud requiere autenticación
+                .anyRequest().authenticated()
+        )
+        .addFilter(new JWTAuthenticationFilter(authenticationManager)) // Agregar el filtro de autenticación JWT
+        .addFilter(new JWTValidationFilter(authenticationManager))
+        .build();
 	}
 	
 	@Bean
@@ -91,7 +143,7 @@ public class SecurityConfig {
 	}
 	
 	@Bean
-	FilterRegistrationBean<CorsFilter> corsFilter(){
+	FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean(){
 		FilterRegistrationBean<CorsFilter> corsBean = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
 		
 		corsBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
