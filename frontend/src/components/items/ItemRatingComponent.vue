bed80cca2a9b3114c602e597c58d67f616b346e6
pblvicente <pvicentebusiness@gmail.com>
2025-05-05 20:20:23 +0200

    [MOD] Implementado un diseño para cuando no hay reviews de un producto en ItemRatingComponent

--- frontend/src/components/items/ItemRatingComponent.vue
+++ frontend/src/components/items/ItemRatingComponent.vue
@@ -2,7 +2,7 @@
 
     <figure
         v-if="itemRating > 0"
-        class="tw-flex tw-align-items-center tw-text-white tw-justify-center tw-py-1.5 tw-px-2 tw-bg-black/80 tw-rounded-lg"
+        class="tw-flex tw-align-items-center tw-text-white tw-justify-center tw-py-1.5 tw-px-2 tw-bg-black/80 tw-rounded-lg tw-cursor-help"
         :title="`${itemRating} / 5 in ${ratingCount} reviews`"
     >
         <div class="tw-font-bold tw-mr-1.5 tw-text-gray-400">
@@ -26,6 +26,20 @@
                 class="bi bi-star-fill tw-text-gray-500"
             ></i>
         </template>
+    </figure>
+
+    <figure
+        v-else
+        class="tw-flex tw-align-items-center tw-text-white tw-justify-center tw-py-1.5 tw-px-2 tw-bg-black/80 tw-rounded-lg tw-cursor-help"
+        :title="`No reviews yet`"
+    >
+        <div class="tw-font-bold tw-text-gray-400 tw-mx-1.5 lg:tw-mx-0">
+            {{ '0' }}
+            <span class="tw-hidden lg:tw-inline tw-truncate">
+                {{ ' reviews' }}
+            </span>
+        </div>
+        
     </figure>
 
 </template>  
