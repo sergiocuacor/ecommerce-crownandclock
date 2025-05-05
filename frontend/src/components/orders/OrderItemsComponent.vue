66c71c7fde919a0a00a0a165b2b624f0a6b8b19c
pblvicente <pvicentebusiness@gmail.com>
2025-05-05 20:32:05 +0200

    [MOD] Añadido un modo administrador a OrderItemsComponent

--- frontend/src/components/orders/OrderItemsComponent.vue
+++ frontend/src/components/orders/OrderItemsComponent.vue
@@ -2,7 +2,7 @@
 
     <ul v-if="props.orderItems.length > 0" class="tw-px-2.5 tw-divide-y-2 tw-divide-slate-400/25 tw-bg-gray-200 tw-border-2 tw-rounded-lg">
         <p class="tw-text-center tw-text-lg tw-font-semibold tw-py-2">{{  'Watches' }}</p>
-        <OrderItemComponent v-for="item, index in props.orderItems" :key="index" :item="item"/>
+        <OrderItemComponent :adminMode="props.adminMode" v-for="item, index in props.orderItems" :key="index" :item="item"/>
     </ul>
 
 </template>
@@ -16,6 +16,10 @@
             type: Array,
             default: [],
             required: true
+        },
+        adminMode: {
+            type: Boolean,
+            default: false,
         }
     });
 
