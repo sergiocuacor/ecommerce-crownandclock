7f1f6c24bbcfae2d04d4f20db2dd773983ad6b78
pblvicente <pvicentebusiness@gmail.com>
2025-05-05 20:25:20 +0200

    [MOD] Cambiado el diseño del número de productos de una marca en BrandComponent

--- frontend/src/components/brands/BrandComponent.vue
+++ frontend/src/components/brands/BrandComponent.vue
@@ -2,7 +2,7 @@
 
     <ElementComponent :toRoute="{ name: 'brand', params: { mask: props.brand.id } }" :imgSrc="`/no_image.png`" :imgAlt="`${props.brand.id}`" :imgCustomization="`object-fit-cover`" :title="props.brand.name">
         <template #top-right>
-            <div :title="`Watches From This Brand`" class="tw-text-white tw-font-bold tw-text-xl tw-tracking-wider tw-bg-black tw-m-2 tw-p-2 tw-border tw-border-red-500 tw-rounded-full">
+            <div :title="`Watches From This Brand`" class="tw-text-white tw-font-bold tw-text-xl tw-tracking-wider tw-bg-black tw-m-2 tw-p-2 tw-border tw-border-red-500 tw-rounded-[8px]">
                 {{ props.brand.numberOfProducts }}
             </div>
         </template>
