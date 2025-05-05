f2851ca236db7fdf46f99a54bbca12fcb5a25d7b
pblvicente <pvicentebusiness@gmail.com>
2025-05-05 20:22:56 +0200

    [MOD] Añadido código para mejorar el diseño cuando un botón está deshabilitado y contemplado que el stock de un producto sea 0 para deshabilitarlo en CartActionsButtonComponent

--- frontend/src/components/cart/CartActionsButtonComponent.vue
+++ frontend/src/components/cart/CartActionsButtonComponent.vue
@@ -3,7 +3,7 @@
     <div class="tw-bg-gray-500/25 tw-flex tw-p-1 tw-rounded-full tw-space-x-1.5">
 
         <button
-            class="tw-bg-white tw-text-black tw-rounded-full tw-w-12 tw-h-12 sm:tw-w-10 sm:tw-h-10 tw-flex tw-items-center tw-justify-center tw-shadow-md hover:tw-bg-gray-100 disabled:tw-bg-gray-200 disabled:tw-text-gray-500 tw-cursor-pointer tw-text-xl tw-transition tw-delay-150 tw-duration-300 tw-ease-in-out hover:tw--translate-y-1 hover:tw-scale-110"
+            class="tw-bg-white tw-text-black tw-rounded-full tw-w-12 tw-h-12 sm:tw-w-10 sm:tw-h-10 tw-flex tw-items-center tw-justify-center tw-shadow-md hover:tw-bg-gray-100 disabled:tw-bg-gray-200 disabled:tw-text-gray-500 tw-cursor-pointer tw-text-xl tw-transition tw-delay-150 tw-duration-300 tw-ease-in-out hover:tw--translate-y-1 hover:tw-scale-110 disabled:hover:tw-scale-90 disabled:hover:tw--translate-y-0 disabled:tw-cursor-not-allowed"
             @click="addToCart"
             :disabled="isDisabledAdd"
             data-bs-toggle="offcanvas"
@@ -15,7 +15,7 @@
     
         <button
             type="button"
-            class="tw-bg-black tw-text-white tw-rounded-full tw-w-12 tw-h-12 sm:tw-w-10 sm:tw-h-10 tw-flex tw-items-center tw-justify-center tw-shadow-md hover:tw-bg-gray-800 disabled:tw-bg-gray-500 disabled:tw-text-gray-200 tw-cursor-pointer tw-text-xl tw-transition tw-delay-150 tw-duration-300 tw-ease-in-out hover:tw--translate-y-1 hover:tw-scale-110"
+            class="tw-bg-black tw-text-white tw-rounded-full tw-w-12 tw-h-12 sm:tw-w-10 sm:tw-h-10 tw-flex tw-items-center tw-justify-center tw-shadow-md hover:tw-bg-gray-800 disabled:tw-bg-gray-500 disabled:tw-text-gray-200 tw-cursor-pointer tw-text-xl tw-transition tw-delay-150 tw-duration-300 tw-ease-in-out hover:tw--translate-y-1 hover:tw-scale-110 disabled:hover:tw-scale-90 disabled:hover:tw--translate-y-0 disabled:tw-cursor-not-allowed"
             @click="removeFromCart"
             :disabled="isDisabledRemove"
             data-bs-toggle="offcanvas"
@@ -50,6 +50,7 @@
     });
 
     const isDisabledAdd = computed(() => {
+        if (props.item.stock <= 0) { return true; }
         const itemInCart = cartStore.items.find(item => item.id === props.item.id);
         return itemInCart ? itemInCart.quantity >= itemInCart.stock : false;
     });
