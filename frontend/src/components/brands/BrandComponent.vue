7a30e377a05966a496a47d6d596c0d7cffd07a34
pblvicente <pvicentebusiness@gmail.com>
2025-05-05 22:45:33 +0200

    [MOD] Rediseño de BrandComponent

--- frontend/src/components/brands/BrandComponent.vue
+++ frontend/src/components/brands/BrandComponent.vue
@@ -1,16 +1,11 @@
 <template>
 
-    <ElementComponent :toRoute="{ name: 'brand', params: { mask: props.brand.id } }" :imgSrc="`/no_image.png`" :imgAlt="`${props.brand.id}`" :imgCustomization="`object-fit-cover`" :title="props.brand.name">
-        <template #top-right>
-            <div :title="`Watches From This Brand`" class="tw-text-white tw-font-bold tw-text-xl tw-tracking-wider tw-bg-black tw-m-2 tw-p-2 tw-border tw-border-red-500 tw-rounded-[8px]">
+    <ElementComponent :toRoute="{ name: 'brand', params: { mask: props.brand.id } }" :imgSrc="`/public/brands/${props.brand.id}.png`" :imgAlt="`${props.brand.id}`" :imgCustomization="`object-fit-contain p-4`" :title="props.brand.name">
+        <template #bottom-right>
+            <div :title="`Watches From This Brand`" class="tw-text-black tw-font-bold tw-text-xl tw-tracking-wider tw-mb-1.5 tw-me-2 tw-cursor-help">
                 {{ props.brand.numberOfProducts }}
             </div>
-        </template>
-        <template #middle-center>
-            <router-link :to="{ name: 'brand', params: { mask: props.brand.id } }" class="tw-block tw-font-bold tw-text-2xl tw-uppercase tw-text-center">
-                {{ props.brand.name }}
-            </router-link>
-        </template>      
+        </template> 
     </ElementComponent>
 
 </template>
