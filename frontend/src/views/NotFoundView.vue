50844ebc3b4e39c62f08ebb342794f8ab1f0317e
pblvicente <pvicentebusiness@gmail.com>
2025-05-05 20:21:11 +0200

    [MOD] Actualizado el diseño de NotFoundView

--- frontend/src/views/NotFoundView.vue
+++ frontend/src/views/NotFoundView.vue
@@ -2,12 +2,12 @@
     <DynamicHeightDivComponent>
         <template #header>
             <div class="tw-font-mono tw-text-3xl lg:tw-text-6xl">
-                <i class="bi bi-bug-fill tw-text-red-600"></i> 404 ERROR <i class="bi bi-bug-fill tw-text-red-600"></i>
+                <i class="bi bi-bug-fill tw-text-red-600 tw-me-1"></i>{{ '404 ERROR' }}<i class="bi bi-bug-fill tw-text-red-600 tw-ms-1"></i>
             </div>
         </template>
         <template #footer >
             <div class="tw-font-mono tw-text-xl lg:tw-text-4xl">
-                Not Found
+                {{ 'Not Found' }}
             </div>
         </template>
     </DynamicHeightDivComponent>
