2ac9d5dacf790b5882579479f8bf1baf81988c88
pblvicente <pvicentebusiness@gmail.com>
2025-05-06 18:22:43 +0200

    [FIX] Arreglada la ruta de las imágenes en BrandComponent

--- frontend/src/components/brands/BrandComponent.vue
+++ frontend/src/components/brands/BrandComponent.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <ElementComponent :toRoute="{ name: 'brand', params: { mask: props.brand.id } }" :imgSrc="`/public/brands/${props.brand.id}.png`" :imgAlt="`${props.brand.id}`" :imgCustomization="`object-fit-contain p-4`" :title="props.brand.name">
+    <ElementComponent :toRoute="{ name: 'brand', params: { mask: props.brand.id } }" :imgSrc="`/brands/${props.brand.id}.png`" :imgAlt="`${props.brand.id}`" :imgCustomization="`object-fit-contain p-4`" :title="props.brand.name">
         <template #bottom-right>
             <div :title="`Watches From This Brand`" class="tw-text-black tw-font-bold tw-text-xl tw-tracking-wider tw-mb-1.5 tw-me-2 tw-cursor-help">
                 {{ props.brand.numberOfProducts }}
